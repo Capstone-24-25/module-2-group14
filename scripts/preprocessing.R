7b292a96d2b3d81fdf9fa444b4db45a72c31f611
Xinlong Yang <xinlong@ucsb.edu>
2024-11-16 15:17:26 -0800

    Update preprocessing.R

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -11,7 +11,7 @@ require(tokenizers)
 # function to parse html and clean text
 parse_fn <- function(.html){
   read_html(.html) %>%
-    html_elements('p') %>%
+    html_elements('p, h1, h2, h3, h4, h5, h6') %>%
     html_text2() %>%
     str_c(collapse = ' ') %>%
     rm_url() %>%
@@ -43,6 +43,28 @@ nlp_fn <- function(parse_data.out){
     unnest_tokens(output = token, 
                   input = text_clean, 
                   token = 'words',
+                  stopwords = str_remove_all(stop_words$word, 
+                                             '[[:punct:]]')) %>%
+    mutate(token.lem = lemmatize_words(token)) %>%
+    filter(str_length(token.lem) > 2) %>%
+    count(.id, bclass, token.lem, name = 'n') %>%
+    bind_tf_idf(term = token.lem, 
+                document = .id,
+                n = n) %>%
+    pivot_wider(id_cols = c('.id', 'bclass'),
+                names_from = 'token.lem',
+                values_from = 'tf_idf',
+                values_fill = 0)
+  return(out)
+}
+
+
+nlp_bigrams <- function(parse_data.out){
+  out <- parse_data.out %>% 
+    unnest_tokens(output = token, 
+                  input = text_clean, 
+                  token = 'ngrams',
+                  n = 2,
                   stopwords = str_remove_all(stop_words$word, 
                                              '[[:punct:]]')) %>%
     mutate(token.lem = lemmatize_words(token)) %>%
